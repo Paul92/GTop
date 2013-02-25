3ca2d6c3bab05e244854eadaf70196ea24eaa2d4
Paul92 <paul.gafton@gmail.com>
2013-02-25 02:10:49 +0200

    HELP

--- drumuire.cpp
+++ drumuire.cpp
@@ -1,10 +1,15 @@
 #include "inout.h"
 #include "error.h"
 #include "maths.h"
+#include<cstring>
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
 
 extern long long errors;
 
 using namespace std;
+
 
 struct point{
     char point_id[100];
@@ -15,6 +20,27 @@ struct point{
     long long theta;
 };
 
+void printPoints(point a[100], int n){
+
+    cout<<"A"<<n;
+
+    for(int i=0; i<n; i++){
+        cout<<i;
+        cout<<a[i].point_id;//<<' '<<a[i].nextDist<<' '<<a[i].nextHz<<'\n';
+    }
+
+}
+
+void copy(char dest[100], char a[100]){
+    int i=0;
+    do{
+        dest[i]=a[i];
+        i++;
+
+    }while(a[i]!=0);
+
+}
+
 int drumuire(int argc, char **argv){
 
     char* inputFileName=argv[1];
@@ -22,6 +48,8 @@ int drumuire(int argc, char **argv){
 
     FILE *inputFile=fopen(inputFileName, "r");
     FILE *outputFile=fopen(outputFileName, "w");
+
+//    cout<<"A";
 
     char line[MAX_LINE];
     char point_id[100];
@@ -32,13 +60,16 @@ int drumuire(int argc, char **argv){
 
     long long dummy=0;
 
-    char lastPoint_id[100];
+    char* temp;//=(char*)malloc(100);
     char currentPoint_id[100];
 
     long long count;
 
-    while(!feof(inputFile)){
-        points[pointsIndex].point_id=readStation(inputFile);
+
+   while(!feof(inputFile)){
+        char* station_id=readStation(inputFile);
+//        cout<<station_id<<'\n';
+        strcpy(points[pointsIndex].point_id, station_id);
         if(!feof(inputFile)){
             count=0;
             if(pointsIndex>0 && strcmp(currentPoint_id, points[pointsIndex].point_id)!=0)
@@ -50,14 +81,24 @@ int drumuire(int argc, char **argv){
                 errors=errors|(1<<6);
             count+=readPoint(inputFile, currentPoint_id,
                       points[pointsIndex].nextDist, points[pointsIndex].nextHz, dummy);
-            pointsIndex++;
-            count+=readPoint(inputFile, currentPoint_id, dummy, dummy);
+//            pointsIndex++;
+            count+=readPoint(inputFile, currentPoint_id, dummy, dummy, dummy);
             if(count!=2){
                 errors=errors|(1<<7);
             }
         }
     }
+//   cout<<points[0].point_id;
 
-    
+    for(int i=0; i<pointsIndex; i++){
+//        cout<<i;
+       fprintf(stderr, "%s", points[i].point_id);
+  }
+
+//    cout<<"A";
+
+  //  free(temp);
+
+    return 0;
 
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "radiere.h"
-//#include "drumuire.h"
+#include "drumuire.h"
 
 #include<cstring>
 #include<cstdio>
@@ -15,7 +15,7 @@ int main(int argc, char **argv){
     }else if(argc>4){
         printf("Prea multe argumente\n");
     }
-    
+
     if(strcmp(argv[1], "-r")==0){
         return radiere(0, argv+1);
     }else if(strcmp(argv[1], "-d")==0){
