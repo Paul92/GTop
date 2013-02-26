1fbac3e92b76fa335a652d0272b67104a194aefe
Paul92 <paul.gafton@gmail.com>
2013-02-26 02:54:26 +0200

    Removed some testing crap

--- drumuire.cpp
+++ drumuire.cpp
@@ -20,27 +20,6 @@ struct point{
     long long theta;
 };
 
-void printPoints(point a[100], int n){
-
-    cout<<"A"<<n;
-
-    for(int i=0; i<n; i++){
-        cout<<i;
-        cout<<a[i].point_id;//<<' '<<a[i].nextDist<<' '<<a[i].nextHz<<'\n';
-    }
-
-}
-
-void copy(char dest[100], char a[100]){
-    int i=0;
-    do{
-        dest[i]=a[i];
-        i++;
-
-    }while(a[i]!=0);
-
-}
-
 int drumuire(int argc, char **argv){
 
     char* inputFileName=argv[1];
@@ -71,12 +50,11 @@ int drumuire(int argc, char **argv){
                 errors=errors|(1<<6);
             count+=readPoint(inputFile, currentPoint_id, 
                       points[pointsIndex].beforeDist, points[pointsIndex].beforeHz, dummy);
-            //pointsIndex++;
             if(pointsIndex>0 && strcmp(currentPoint_id, points[pointsIndex-1].point_id)!=0)
                 errors=errors|(1<<6);
             count+=readPoint(inputFile, currentPoint_id,
                       points[pointsIndex].nextDist, points[pointsIndex].nextHz, dummy);
-
+            theta=
             pointsIndex++;
             count+=readPoint(inputFile, currentPoint_id, dummy, dummy, dummy);
             if(count!=2){
@@ -85,10 +63,6 @@ int drumuire(int argc, char **argv){
         }
     }
 
-    for(int i=0; i<pointsIndex-1; i++){
-       printf("%s %lld %lld %lld %lld\n", points[i].point_id, points[i].nextDist, points[i].nextHz,
-                                            points[i].beforeDist, points[i].beforeHz );
-    }
 
 
     return 0;
