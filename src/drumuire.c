2b6186b2fcfd8d6decd1a78516ff8f85bbd4b4b0
Paul92 <paul.gafton@gmail.com>
2013-04-10 21:56:20 +0300

    reading in drumuire.c

--- src/drumuire.c
+++ src/drumuire.c
@@ -11,114 +11,112 @@ struct point{
     char point_id[100];
     double nextDist;
     double nextHz;
-    double beforeDist;
     double beforeHz;
+    double beforeDist;
     double beta;
     double theta;      //between this point and the next one
+    struct point* next;
+    struct point* before;
 };
+
+void nodeInit(struct point *node){
+    node->next=NULL;
+    node->before=NULL;
+}
+
+void addElement(struct point *node, struct point *data){
+    
+    while(node->next!=NULL){
+        addElement(node->next, data);
+    }
+    node->next=data;
+    data->before=node;
+}
 
 int drumuire(int argc, char **argv){
 
-/*    char* inputFileName=argv[1];
+    char* inputFileName=argv[1];
     char* outputFileName=argv[2];
 
     FILE *inputFile=fopen(inputFileName, "r");
     FILE *outputFile=fopen(outputFileName, "w");
 
-    struct point points[100];
-    int pointsIndex=1;
+    struct point *points=(struct point*)malloc(sizeof(struct point));
 
-    char dummyChar[100];
     char line[100];
     int errors=0;
 
     double stationX, stationY, orientX, orientY;
 
-    printf("Coordonata X a statiei de plecare %lf este: ", stationX);
+    char* station_id;
+    station_id=readStation(inputFile);
+
+    printf("Coordonata X a statiei de plecare %s este: ", station_id);
     fscanf(stdin, "%s", line);
     stationX=readNumber(line);
 
-    printf("Coordonata Y a statiei de plecare %lf este: ", stationY);
+    printf("Coordonata Y a statiei de plecare %s este: ", station_id);
     fscanf(stdin, "%s", line);
     stationY=readNumber(line);
 
-    printf("Coordonata X a bornei de orientare %lf este: ", orientX);
+    strcpy(station_id, points->point_id);
+
+    printf("Coordonata X a bornei de orientare este: ");
     fscanf(stdin, "%s", line);
     orientX=readNumber(line);
 
-    printf("Coordonata Y a bornei de orientare %lf este: ", orientY);
+    printf("Coordonata Y a bornei de orientare este: ");
     fscanf(stdin, "%s", line);
     orientY=readNumber(line);
 
-    char currentPoint_id[100];
+    char orient_id[100];
+    double orientHz;
 
-    int count=0;
-    double dummy;
+    readPoint(inputFile, orient_id, orientHz);
 
-    struct point orient;
+    struct point *firstPoint=(struct point*)malloc(sizeof(struct point));
 
-    char* station_id;
-    station_id=readStation(inputFile);
-    strcpy(points[0].point_id, station_id);
-    count+=readPoint(inputFile, currentPoint_id, &orient.beforeDist, &orient.beforeHz, &dummy);
-    strcpy(orient.point_id, currentPoint_id);
-    count+=readPoint(inputFile, currentPoint_id, &points[0].nextDist, &points[0].nextHz, &dummy);
-    count+=readPoint(inputFile, currentPoint_id, &dummy, &dummy, &dummy);
+    readPoint(inputFile, firstPoint->point_id, &firstPoint->beforeDist, 
+              &firstPoint->nextHz);
+// hv is optional argument
 
-    if(count!=2)
-        errors++;
+    readPoint(inputFile);
+//actually make all argument optional. I don't like dummys. This is for the
+//first stop
+
+//here goes some error checking, based on return value of readPoint
 
     double alpha;
-    alpha=abs(points[0].nextHz-orient.beforeHz);
+    alpha=abs(points->nextHz-orientHz);
     alpha=repairAngle(alpha);
 
     double betaSum=0;
-    
+
+    char beforeCurrent_id[100], afterCurrent_id[100]; //used for error check
+
     while(!feof(inputFile)){
-        station_id=readStation(inputFile);
+        struct point *newPoint=(struct point*)malloc(sizeof(struct point));
+        newPoint->point_id=readStation(inputFile);
         if(!feof(inputFile)){
-            strcpy(points[pointsIndex].point_id, station_id);
-            count=0;
-
-            if(pointsIndex>0 && strcmp(currentPoint_id, points[pointsIndex].point_id)!=0)
-                errors++;
-            count+=readPoint(inputFile, currentPoint_id, 
-                      &points[pointsIndex].beforeDist, &points[pointsIndex].beforeHz, &dummy);
             
-            if(pointsIndex>0 && strcmp(currentPoint_id, points[pointsIndex-1].point_id)!=0)
-                errors++;
-            count+=readPoint(inputFile, currentPoint_id,
-                      &points[pointsIndex].nextDist, &points[pointsIndex].nextHz, &dummy);
+            readPoint(inputFile, beforeCurrent_id, &newPoint->beforeDist, 
+                      &newPoint->beforeHz);
             
-            points[pointsIndex].beta=abs(points[pointsIndex].beforeHz-points[pointsIndex].nextHz);
-            points[pointsIndex].beta=repairAngle(points[pointsIndex].beta);
+            readPoint(inputFile, afterCurrent_id, &newPoint->nextDist,
+                      &newPoint->nextHz);
+            
+            newPoint->beta=abs(newPoint->beforeHz-newPoint->nextHz);
+            newPoint->beta=repairAngle(newPoint->beta);
 
-            betaSum+=points[pointsIndex].beta;
+            betaSum+=newPoint->beta;
 
-            pointsIndex++;
-            count+=readPoint(inputFile, dummyChar, &dummy, &dummy, &dummy);
-            if(count!=2){
-                errors++;
-            }
+            count+=readPoint(inputFile);    //check for stop
+                                            //and for other damn errors
+            addElement(points, newPoint);
         }
     }
 
 
-
-/*    points[0].beforeDist=points[pointsIndex-1].nextDist;
-    points[0].beforeHz=points[pointsIndex-1].nextHz;
-    points[0].beta=abs(points[0].beforeHz-points[0].nextHz);
-    points[0].beta=repairAngle(points[0].beta);
-
-    double correction=200*(pointsIndex-2)-betaSum;
-
-    double th=theta(stationX, stationY, orientX, orientY);
-    points[0].theta=ac+alpha;
-
-    for(int i=0; i<pointsIndex; i++){
-        points[i].beta+=correction;
-    }
-*/
     return 0;
 
 }
